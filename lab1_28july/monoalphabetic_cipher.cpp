fd89f538aecd78be7c88b317f3897ab22758c98d
Rahul <44724414+rahulverma2110@users.noreply.github.com>
2019-08-04 23:39:31 +0530

    Update monoalphabetic_cipher.cpp

--- lab1_28july/monoalphabetic_cipher.cpp
+++ lab1_28july/monoalphabetic_cipher.cpp
@@ -7,14 +7,55 @@ using namespace std;
 
 int main()
 {
-	string key = "QWERTYUIOPASDFGHJKLZXCVBNM";
-	
+	string key;
 	ifstream fio;
 	ofstream of;
 	//cout<<s.length();
 	int flag;
-	cout<<"Enter 0 for encryption and 1 for decryption: ";
-	cin>>flag;
+	//checking the length of the string
+	int f = 1;
+	while(f == 1)
+	{
+		
+		cout<<"Enter 0 for Encryption and 1 for Decryption: ";
+		cin>>flag;
+		cout<<"Enter the key string with distinct 26 alphabets:"<<endl;
+		cin>>key;
+		if(key.length() != 26)
+		{
+			cout<<"Invalid string!!! Please input a valid string with exactly 26 alphabets"<<endl;
+			f = 1;
+		}
+		else 
+		{
+			int arr[26];
+			int i;
+			int g;
+			for(i = 0; i<26; i++)
+			{
+				arr[i] = 0;
+			}
+
+			for(i = 0; i<26; i++)
+			{
+				g = key[i] - 65;
+			
+				if (arr[g] == 0)
+				{
+					arr[g] = 1;
+					f = 0;
+				}
+				else
+				{
+					cout<<"Invalid string!!! Please input a valid string with no repetition of alphabets\n";
+					f = 1;
+					break;
+				}
+			
+			}		
+		}
+	}
+	
 	if(flag==0)
 	{
 		fio.open("file.txt");
@@ -22,11 +63,10 @@ int main()
 		getline(fio,s);
 	//	cout<<s<<endl;
 		fio.close();
-		for(int i = 0;i < s.length();i++)
+		for(i = 0;i < s.length();i++)
 		{
 			if(s[i] == ' ')
 			{
-			i++;
 			continue;
 			}
 			s[i] = key[s[i] - 97];
@@ -44,11 +84,12 @@ int main()
 		string s;
 		getline(fio,s);
 		//cout<<s<<endl;
-		for(int j = 0;j <= s.length(); j++)
+		for(j = 0;j <= s.length(); j++)
 		{
 			if(s[j] == ' ')
-			{j++;
-			continue;}
+			{
+				continue;
+			}
 			int k;
 			for(k = 0; k<key.length(); k++)
 			{
